ec6c456b9570e362bf0cb2dd6058b736d7129c86
qonv <112008535+qonv@users.noreply.github.com>
2021-02-27 06:27:31 +0000

    New file structure

--- data/clustering/arff2csv.py
+++ data/clustering/arff2csv.py
@@ -2,4 +2,15 @@ from scipy.io import arff
 import sys
 import numpy
 import os
-import pand
+import pandas
+
+fname = sys.argv[1]
+basename = os.path.splitext(fname)
+f = open(fname)
+data, meta = arff.loadarff(f)
+print meta
+#numpy.savetxt(basename[0]+".csv", data, delimiter=",")
+print basename
+#data.tofile(basename[0]+".csv", sep=',', format='%10.5f')
+
+df = pandas.Dat
