9019127ac52ddf7a6713a4127429954ea9083258
qonv <112008535+qonv@users.noreply.github.com>
2021-09-11 12:23:40 +0000

    Update impressum.md

--- python/connect4_timing.py
+++ python/connect4_timing.py
@@ -8,3 +8,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import KFold
 import collections
 from sklearn import preprocessing
+from sklearn.utils import check_random_state
+from sklearn.feature_extraction import DictVectorizer
+from sklearn import tree
+import time
+import sys
+
+data = pandas.read_table("data/connect-4.csv", header=0, sep=",")
+
+cvt = data.columns
+targetcol
