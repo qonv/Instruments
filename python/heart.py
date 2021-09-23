74dfb0fc9674e37d6b85ac6428c69714ccd3e70e
qonv <112008535+qonv@users.noreply.github.com>
2021-09-23 06:56:13 +0000

    KISS

--- python/heart.py
+++ python/heart.py
@@ -1,3 +1,9 @@
 from sklearn import tree
 import pandas
-from sklearn.ense
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+import collections
+from sklearn import preprocessing
+from sklearn.utils import check_random_state
+from sklearn.feature_extraction import DictVectorizer
+from sklearn import tr
