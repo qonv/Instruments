98d66abf3f17fc4a5c44abb0e4cba84965e7950d
qonv <112008535+qonv@users.noreply.github.com>
2021-09-29 22:30:05 +0000

    Remove unnecessary require

--- python/shuttle_timing.py
+++ python/shuttle_timing.py
@@ -2,4 +2,12 @@
 $ python python/connect4_timing.py 50 20
 """
 
-from sklearn 
+from sklearn import tree
+import pandas
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+import collections
+from sklearn import preprocessing
+from sklearn.utils import check_random_state
+from sklearn.feature_extraction import DictVectorizer
+from sklear
