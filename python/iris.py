815c466d2355ad2c5dd30c384cda93e354a59d40
qonv <112008535+qonv@users.noreply.github.com>
2021-09-29 06:45:07 +0000

    performance fix

--- python/iris.py
+++ python/iris.py
@@ -4,4 +4,11 @@ from sklearn.model_selection import KFold
 import collections
 import numpy as np
 import pandas
-f
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder()
+
+#data = datasets.load_iris()
+data = pandas.read_table("../data/iris.csv", header=0, sep=",")
+
+# convert target column from string to int 0..2
+data = data[data.columns].apply(lambda x : pan
