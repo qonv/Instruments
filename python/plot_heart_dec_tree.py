ea1fdc76db43f34fbb319840e2a3b35f91e4a519
qonv <112008535+qonv@users.noreply.github.com>
2021-09-29 15:07:08 +0000

    update method

--- python/plot_heart_dec_tree.py
+++ python/plot_heart_dec_tree.py
@@ -2,4 +2,11 @@ from sklearn import tree
 import pydotplus 
 from sklearn.datasets.mldata import fetch_mldata
 
-heart = fetch_mldat
+heart = fetch_mldata('Heart')
+
+clf = tree.DecisionTreeClassifier(criterion="entropy")
+clf = clf.fit(heart.data, heart.target)
+
+names = ["id","Age","Sex","ChestPain","RestBP","Chol","Fbs","RestECG","MaxHR","ExAng","Oldpeak","Slope","Ca","Thal","AHD"]
+
+dot_data = tree.export_graphviz(clf, feature_n
