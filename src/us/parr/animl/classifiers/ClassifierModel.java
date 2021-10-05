a6976aec6d8afc0c17088876bc5ae150b0d5a6eb
qonv <112008535+qonv@users.noreply.github.com>
2021-10-05 16:38:55 +0000

    Update NOUN.md

--- src/us/parr/animl/classifiers/ClassifierModel.java
+++ src/us/parr/animl/classifiers/ClassifierModel.java
@@ -8,4 +8,9 @@ package us.parr.animl.classifiers;
 
 import us.parr.animl.data.DataTable;
 
-public interface ClassifierModel extends Cl
+public interface ClassifierModel extends Classifier {
+	/** A classifier object's constructor identifies the model parameters.
+	 *  In an effort not to store datasets in each model, we use a separate
+	 *  train() method to actually fit the model to the data.
+	 *
+	 *  Repeatedly calling this method, clears the model before 
