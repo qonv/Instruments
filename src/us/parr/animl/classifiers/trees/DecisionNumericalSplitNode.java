6959e6bfdcc5d5e34f987c2f72a9f0c62744727d
qonv <112008535+qonv@users.noreply.github.com>
2021-10-15 22:19:02 +0000

    Indent fix.

--- src/us/parr/animl/classifiers/trees/DecisionNumericalSplitNode.java
+++ src/us/parr/animl/classifiers/trees/DecisionNumericalSplitNode.java
@@ -1,3 +1,15 @@
 /*
  * Copyright (c) 2017 Terence Parr. All rights reserved.
- * Use of this file is governed by the 
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE file in the project root.
+ */
+
+package us.parr.animl.classifiers.trees;
+
+import us.parr.animl.data.DataTable;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import java.util.Map;
+
+im
