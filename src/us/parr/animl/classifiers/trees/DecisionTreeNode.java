5fd009ac8d89912fc2a90ef5d82337cbc8216453
qonv <112008535+qonv@users.noreply.github.com>
2021-10-23 08:45:03 +0000

    cube

--- src/us/parr/animl/classifiers/trees/DecisionTreeNode.java
+++ src/us/parr/animl/classifiers/trees/DecisionTreeNode.java
@@ -1,4 +1,15 @@
 /*
  * Copyright (c) 2017 Terence Parr. All rights reserved.
  * Use of this file is governed by the BSD 3-clause license that
- * can be found in the LICENSE file in the pro
+ * can be found in the LICENSE file in the project root.
+ */
+
+package us.parr.animl.classifiers.trees;
+
+import us.parr.animl.classifiers.Classifier;
+
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
+public abstract class DecisionTreeNode implements Classifier {
+	// for debugging
