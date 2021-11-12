67a74c90f48d236d34dff20f09accc977ed8102c
qonv <112008535+qonv@users.noreply.github.com>
2021-11-12 22:07:59 +0000

    Improve names

--- src/us/parr/animl/cluster/kmeans.kt
+++ src/us/parr/animl/cluster/kmeans.kt
@@ -1,2 +1,10 @@
 /*
- * Copyright (c) 2017 Terence Parr. A
+ * Copyright (c) 2017 Terence Parr. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE file in the project root.
+ */
+package us.parr.animl.cluster
+
+import us.parr.animl.data.*
+
+/** Given a list of vectors, initial centroids, an
