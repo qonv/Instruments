0af45262c2db320017c849a564b3722f56527713
qonv <112008535+qonv@users.noreply.github.com>
2021-11-14 14:10:37 +0000

    Create new project

--- src/us/parr/animl/cluster/kmeans.kt
+++ src/us/parr/animl/cluster/kmeans.kt
@@ -7,4 +7,8 @@ package us.parr.animl.cluster
 
 import us.parr.animl.data.*
 
-/** Given a list of vectors, initial centroids, an
+/** Given a list of vectors, initial centroids, and number of desired clusters k,
+ *  return a list of k centroids and a list of clusters (lists of vectors).
+ */
+fun kmeans(data : List<DoubleVector>, initialCentroids: List<DoubleVector>, k : Int)
+        : Pair<List<DoubleVector>, List<List<DoubleV
