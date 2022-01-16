80de3f55693063b6985f4011564477edfceac790
qonv <112008535+qonv@users.noreply.github.com>
2022-01-16 16:32:53 +0000

    database fix

--- src/us/parr/animl/data/DataPair.java
+++ src/us/parr/animl/data/DataPair.java
@@ -1,3 +1,14 @@
 /*
  * Copyright (c) 2017 Terence Parr. All rights reserved.
- * Use of this file is gover
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE file in the project root.
+ */
+
+package us.parr.animl.data;
+
+public class DataPair {
+	public DataTable region1;
+	public DataTable region2;
+
+	public DataPair(DataTable region1, DataTable b) {
+		this.
