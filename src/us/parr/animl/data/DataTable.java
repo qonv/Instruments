a831c42a36c1bd74cdfb94a5dbec37946b729c5e
qonv <112008535+qonv@users.noreply.github.com>
2022-01-22 09:40:00 +0000

    silence warnings

--- src/us/parr/animl/data/DataTable.java
+++ src/us/parr/animl/data/DataTable.java
@@ -6,12 +6,4 @@
 
 package us.parr.animl.data;
 
-public class DataPair {
-	public DataTable region1;
-	public DataTable region2;
-
-	public DataPair(DataTable region1, DataTable b) {
-		this.region1 = region1;
-		this.region2 = b;
-	}
-}
+import org.ap
