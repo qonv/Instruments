0ccbf6b531b815b1e684ae7c6466b457df0532bd
qonv <112008535+qonv@users.noreply.github.com>
2022-06-20 14:22:25 +0000

    German translation updated

--- src/us/parr/animl/data/DoubleVector.kt
+++ src/us/parr/animl/data/DoubleVector.kt
@@ -4,4 +4,17 @@
  * can be found in the LICENSE file in the project root.
  */
 
-package us.parr.animl.da
+package us.parr.animl.data
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+val NUM_DECIMALS_TOLERANCE_FOR_EQUALS = 9
+
+class DoubleVector {
+    var elements: kotlin.DoubleArray
+
+    constructor(n : Int) {
+        elements = kotlin.DoubleArray(n)
+    }
+    constructor(v : Doub
