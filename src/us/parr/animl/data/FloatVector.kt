3e8fe99c393f25a5cf557a4cf6661d4f9b4a75c0
qonv <112008535+qonv@users.noreply.github.com>
2022-07-29 14:54:47 +0000

    Create tmp.txt

--- src/us/parr/animl/data/FloatVector.kt
+++ src/us/parr/animl/data/FloatVector.kt
@@ -1,2 +1,12 @@
 /*
- * Copyright (c) 2017 Tere
+ * Copyright (c) 2017 Terence Parr. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE file in the project root.
+ */
+
+package us.parr.animl.data
+
+import java.lang.Math.abs
+import java.lang.Math.max
+
+/** A simple vector to 
