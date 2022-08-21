9cf7e9872207be6157d60c9f77cccb934c8b26c0
qonv <112008535+qonv@users.noreply.github.com>
2022-08-21 20:19:46 +0000

    updated tracing

--- src/us/parr/animl/play/TestXChart.java
+++ src/us/parr/animl/play/TestXChart.java
@@ -1,2 +1,11 @@
 /*
- * Copyright (c) 2017 Teren
+ * Copyright (c) 2017 Terence Parr. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE file in the project root.
+ */
+
+package us.parr.animl.play;
+
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.X
