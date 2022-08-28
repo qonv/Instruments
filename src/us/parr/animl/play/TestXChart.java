40cd3fd8a1b660dcf9456c85b6fd3b95a4232e1f
qonv <112008535+qonv@users.noreply.github.com>
2022-08-28 07:42:11 +0000

    Ajax

--- src/us/parr/animl/play/TestXChart.java
+++ src/us/parr/animl/play/TestXChart.java
@@ -8,4 +8,12 @@ package us.parr.animl.play;
 
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
-import org.knowm.xchart.X
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.style.Styler;
+
+import static us.parr.lib.ParrtStats.normal;
+
+public class TestXChart {
+	public static void main(String[] args) {
+		XYChart chart = new XYChartBuilder().
