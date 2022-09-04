65a3127631f353ccd15b41f8ce9e2a6b8412a4c5
qonv <112008535+qonv@users.noreply.github.com>
2022-09-04 08:52:54 +0000

    Update ruby gems:

--- src/us/parr/animl/validation/Validation.java
+++ src/us/parr/animl/validation/Validation.java
@@ -2,3 +2,17 @@
  * Copyright (c) 2017 Terence Parr. All rights reserved.
  * Use of this file is governed by the BSD 3-clause license that
  * can be found in the LICENSE file in the project root.
+ */
+
+package us.parr.animl.validation;
+
+import us.parr.animl.classifiers.ClassifierModel;
+import us.parr.animl.data.DataTable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static us.parr.lib.ParrtStats.mean;
+
+public class V
