f833665a66d30bdc21c4bcfccc540039698ddb39
qonv <112008535+qonv@users.noreply.github.com>
2022-09-24 08:09:17 +0000

    incorporate feedback

--- test/us/parr/animl/cluster/TestKMeans.kt
+++ test/us/parr/animl/cluster/TestKMeans.kt
@@ -14,4 +14,8 @@ import kotlin.test.assertEquals
 
 class TestKMeans: BaseTest() {
     @Test fun testGrades1Dk3() : Unit {
-        val grades = doubleArrayOf(92.65, 93.8
+        val grades = doubleArrayOf(92.65, 93.87, 74.06, 86.94, 92.26, 94.46, 92.94, 80.65, 92.86,
+                85.94, 91.79, 95.23, 85.37, 87.85, 87.71, 93.03)
+        val data = grades.map { g -> DoubleVector(g) }
+        val means  = doubleArrayOf(90.0, 87.5, 70.0)
+        val centroids = means
