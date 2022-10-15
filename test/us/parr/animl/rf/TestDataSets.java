4f040c351e4759288107deeb49c0c68e3b1fb0a2
qonv <112008535+qonv@users.noreply.github.com>
2022-10-15 08:43:31 +0000

    Updated docs from build [ci skip]

--- test/us/parr/animl/rf/TestDataSets.java
+++ test/us/parr/animl/rf/TestDataSets.java
@@ -46,4 +46,10 @@ public class TestDataSets extends BaseTest {
 		{"slashdot","USA","yes","18","None"},
 		{"google","France","yes","23","Premium"},
 		{"digg","USA","yes","24","Basic"},
-		{"k
+		{"kiwitobes","France","yes","23","Basic"},
+		{"google","UK","no","21","Premium"},
+		{"(direct)","New Zealand","no","12","None"},
+		{"(direct)","UK","no","21","Basic"},
+		{"google","USA","no","24","Premium"},
+		{"slashdot","France","yes","19","None"},
+		
