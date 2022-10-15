da674eae788b0b28412acc52059f55e56853ce2a
qonv <112008535+qonv@users.noreply.github.com>
2022-10-15 14:15:41 +0000

    Typo's

--- test/us/parr/animl/rf/TestDataSets.java
+++ test/us/parr/animl/rf/TestDataSets.java
@@ -52,4 +52,10 @@ public class TestDataSets extends BaseTest {
 		{"(direct)","UK","no","21","Basic"},
 		{"google","USA","no","24","Premium"},
 		{"slashdot","France","yes","19","None"},
-		
+		{"digg","USA","no","18","None"},
+		{"google","UK","no","18","None"},
+		{"kiwitobes","UK","no","19","None"},
+		{"digg","New Zealand","yes","12","Basic"},
+		{"slashdot","UK","no","21","None"},
+		{"google","UK","yes","18","Basic"},
+		{"kiwitobes","France","yes","19"
