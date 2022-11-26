6e75935cee57ce7558280db96b59796780b2ba49
qonv <112008535+qonv@users.noreply.github.com>
2022-11-26 19:03:57 +0000

    [Tickets PRO]

--- test/us/parr/animl/rf/TestDecisionTreeBasics.java
+++ test/us/parr/animl/rf/TestDecisionTreeBasics.java
@@ -1,3 +1,15 @@
 /*
  * Copyright (c) 2017 Terence Parr. All rights reserved.
- * Use of this file is governed by the BSD 3-clause lice
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE file in the project root.
+ */
+
+package us.parr.animl.rf;
+
+import org.junit.Test;
+import us.parr.animl.BaseTest;
+import us.parr.animl.classifiers.trees.DecisionTree;
+import us.parr.animl.data.DataTable;
+
+import java.util.ArrayList;
+import 
