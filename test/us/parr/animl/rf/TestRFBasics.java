bd2445391c667f0cc57300fc55ffe120162be5b5
qonv <112008535+qonv@users.noreply.github.com>
2023-02-11 21:22:39 +0000

    Updated graphics

--- test/us/parr/animl/rf/TestRFBasics.java
+++ test/us/parr/animl/rf/TestRFBasics.java
@@ -1,2 +1,12 @@
 /*
- * Copyright (c) 2017 Terence Parr. All rights reserve
+ * Copyright (c) 2017 Terence Parr. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE file in the project root.
+ */
+
+package us.parr.animl.rf;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import us.parr.anim
